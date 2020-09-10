460de033da4c08c8389957cc8752c97f76688cce
wodwine <wodwine@hotmail.com>
2020-09-10 14:25:11 +0700

    Add AtmUI to the code

--- src/main/java/th/ac/ku/atm/Main.java
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -10,7 +10,7 @@ public class Main {
         ApplicationContext context = new AnnotationConfigApplicationContext(ATMConfig.class);
         ATM atm = context.getBean(ATM.class);
 
-        String name = atm.validateCustomer(2, 2345);
-        System.out.println(name + " has " + atm.getBalance());
+        AtmUI atmUI = new AtmUI(atm);
+        atmUI.run();
     }
 }
